42b51395f92a1e967e6637b4042a385d47f7c699
KoharaKazuya <kohara@fenrir.co.jp>
2018-09-10 12:38:04 +0900

    fix: panic when returning nil as error

--- conigo.go
+++ conigo.go
@@ -147,8 +147,8 @@ func (c *Container) construct(constructor interface{}) (reflect.Value, error) {
 
 	constructorReturns := reflect.ValueOf(constructor).Call(argValues)
 	if len(constructorReturns) > 1 {
-		if err, ok := constructorReturns[1].Interface().(error); !ok || err != nil {
-			return reflect.Value{}, err
+		if !constructorReturns[1].IsNil() {
+			return reflect.Value{}, constructorReturns[1].Interface().(error)
 		}
 	}
 
